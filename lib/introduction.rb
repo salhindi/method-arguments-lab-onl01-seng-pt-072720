5129f5fc8b0e4a1165938dd6435977d9517e690e
Sara A <calisaraalhindi@gmail.com>
2020-07-30 19:00:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,8 +2,8 @@ def introduction(name)
  puts "Hi, my name is #{name}." 
 end
 
-def introduction_with_language(language, name)
-  puts name + "," + language
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to "
 end
 
 binding.pry
